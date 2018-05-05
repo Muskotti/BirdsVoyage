0508f59def07dcf148ac35a9ab4c40fca2e02bd8
Muskotti <toni.vanttinen@cs.tamk.fi>
2018-05-05 14:27:46 +0300

    Java doc for enemy

--- core/src/fi/tamk/tiko/Enemy.java
+++ core/src/fi/tamk/tiko/Enemy.java
@@ -1,27 +1,52 @@
 package fi.tamk.tiko;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 
+/**
+ * class for the enemy bird
+ *
+ * @author Toni Vänttinen & Jimi Savola
+ * @version 1.8, 05/02/18
+ * @since 1.8
+ */
 public class Enemy implements EnemyProperties {
+
+    // Enemy sprite
     private Sprite enemy;
+
+    // Enemy rectangle
     private Rectangle enemyRect;
+
+    // Value for the enemies speed
     float speed = 3f;
+
+    // boolean to check if the enemy can move
     boolean moveStop;
 
-    // Enemy constructor
+    /**
+     * Enemy constructor
+     */
     public Enemy() {
         moveStop = true;
         enemy = new Sprite();
         enemyRect = new Rectangle(0,0,256,128);
     }
 
+    /**
+     * Spawns the enemy at one of the 3 spots
+     *
+     * First a random number between 1-3 is generate
+     * Than the given y position is moved
+     * After the random value is checked
+     * depending on the random value the given x value is moved
+     * Finally the enemies position is set
+     * @param x value for the enemies x axis
+     * @param y value for the enemies y axis
+     */
     public void spawn(float x, float y) {
         int random = MathUtils.random(1,3);
         y = y + (enemyHeight * 7);
@@ -38,6 +63,15 @@ public class Enemy implements EnemyProperties {
         enemy.setPosition(x, y);
     }
 
+    /**
+     * Draws the enemy
+     *
+     * First the enemies animation value is changed
+     * next the enemies rectangle is moved where the sprite is
+     * next the enemies animation frame is changed
+     * lastly the animation is drawn
+     * @param host used to make the animation possible
+     */
     public void draw(BirdsVoyage host) {
         host.stateTime += Gdx.graphics.getDeltaTime();
         enemyRect.setPosition(enemy.getX(),enemy.getY());
@@ -45,33 +79,61 @@ public class Enemy implements EnemyProperties {
         host.batch.draw(currentFrame, enemy.getX(), enemy.getY());
     }
 
+    /**
+     * Moves the enemy
+     *
+     * First the code checks if the game is paused
+     * if not the enemy is moved downwards
+     * otherwise it doesn't do anything
+     */
     public void move() {
         if(moveStop) {
             enemy.translateY(-speed);
         }
     }
 
-    public float getEnemyWidth() {
-        return enemyWidth;
-    }
+    /**
+     * Getter for the enemies height
+     * @return gives the enemies height
+     */
     public float getEnemyHeight() {
         return enemyHeight;
     }
 
+    /**
+     * Getter for the enemies y position
+     * @return enemies y position
+     */
     public float getEnemyYpos() {
         return enemy.getY();
     }
+
+    /**
+     * Getter for the enemies x position
+     * @return enemies x position
+     */
     public float getEnemyXpos() {
         return enemy.getX();
     }
+
+    /**
+     * Getter for the enemies rectangle
+     * @return enemies rectangle
+     */
     public Rectangle getEnemyBoundingRectangle() {
         return enemyRect;
     }
 
+    /**
+     * Changes the enemies movement to move
+     */
     public void stop() {
         moveStop = false;
     }
 
+    /**
+     * Changes the enemies movement to stop
+     */
     public void resume() {
         moveStop = true;
     }
